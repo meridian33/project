d367e478dbfede2fd6757bfe96043f2f841caaac
Meridian <adylzhan99@gmail.com>
2025-05-28 16:05:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+// const isChecked = true,
+//       isClose = true;
+
+// console.log(isChecked || !isClose);
+
