7c591656b98beb5759954939a964f8963d0abbce
Meridian <adylzhan99@gmail.com>
2025-06-02 06:57:40 +0500

    Tasks on the topic of logical operators

--- js/script.js
+++ js/script.js
@@ -1,50 +1,35 @@
-// const hamburger = false;
-// const fries = false;
-
-// if (hamburger && fries) {
-//     console.log('Im full');
-// } else if (hamburger) {
-//     console.log("Im 75% full");
-// } else if (fries) {
-//     console.log("im 25% full");
-// } else {
-//     console.log("im hungry");
-// }
 
 
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 0;
-
-// console.log((hamburger === 3 && cola && fries));
-
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && "qfqwefqf");
-
-// if (hamburger === 3 && cola === 1 && fries) {
-//     console.log("Everyones glad");
-// } else {
-//     console.log("Everyones goes to another establishment");
-// }
+console.log( NaN || 2 || undefined ); // 2
 
 
-const hamburger = 3;
-const fries = 2;
+console.log( NaN && 2 && undefined ); // NaN
+
+
+console.log( 1 && 2 && 3 ); // 3
+
+
+console.log( !1 && 2 || !3 ); // !1 (false)
+
+
+console.log( 25 || null && !3 ); // 25
+
+
+console.log( NaN || null || !3 || undefined || 5); // 5
+
+
+console.log( NaN || null && !3 && undefined || 5); // 5
+
+
+console.log( 5 === 5 && 3 > 1 || 5); // 5 === 5 && 3 > 1 (true)
+
+
+let hamburger;
+const fries = NaN;
 const cola = 0;
 const nuggets = 2;
 
-if (hamburger === 3 && cola === 2 || fries === 2 && nuggets) {
-    console.log("Everyones glad");
-} else {
-    console.log("Everyones goes to another establishment");
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
-
-console.log(hamburger === 3 && cola === 2 || fries === 2 && nuggets);
-
-let fierstReport, secondReport, thirdReport, fourthReport = 'done';
-
-console.log(fierstReport || secondReport || thirdReport || fourthReport);
-
-console.log(!0);
