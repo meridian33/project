1f34ad7e699b02cfc6a4e6af82c7c87557b204c8
Meridian <adylzhan99@gmail.com>
2025-06-02 06:47:28 +0500

    logiacal operators &&, ||, !

--- js/script.js
+++ js/script.js
@@ -12,19 +12,39 @@
 // }
 
 
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log((hamburger === 3 && cola && fries));
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "qfqwefqf");
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log("Everyones glad");
+// } else {
+//     console.log("Everyones goes to another establishment");
+// }
+
+
 const hamburger = 3;
-const fries = 1;
+const fries = 2;
 const cola = 0;
+const nuggets = 2;
 
-console.log((hamburger === 3 && cola && fries));
-
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(0 && "qfqwefqf");
-
-if (hamburger === 3 && cola === 1 && fries) {
+if (hamburger === 3 && cola === 2 || fries === 2 && nuggets) {
     console.log("Everyones glad");
 } else {
     console.log("Everyones goes to another establishment");
 }
+
+console.log(hamburger === 3 && cola === 2 || fries === 2 && nuggets);
+
+let fierstReport, secondReport, thirdReport, fourthReport = 'done';
+
+console.log(fierstReport || secondReport || thirdReport || fourthReport);
+
+console.log(!0);
